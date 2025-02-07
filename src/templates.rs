f41b8499b126f14a9a5bafa1f9ff37f6ed6fd8b4
Paddyk45 <patrickk0@tutanota.com>
2025-02-07 12:24:39 +0100

    fix bad html

--- src/templates.rs
+++ src/templates.rs
@@ -138,34 +138,36 @@ pub async fn index_template(cookies: CookieJar) -> (StatusCode, Markup) {
 
     let render = html! {
         (DOCTYPE);
-        head {
-            (NEWCSS)
-            script src="/index.js" {};
-            title { (CONFIG.get().unwrap().lock().await.instance_name) }
-        }
-
-        body {
-            header {
-                h1 { (CONFIG.get().unwrap().lock().await.instance_name) }
-                div style="position: absolute; top: 5px; right: 5px" {
-                    @if !is_logged_in {
-                        label for="password" { "Login: " }
-                        input #password placeholder="Password" type="password";
-                        button style="background: #181818" onclick="onLogin()" { "OK" }
-                    } @else {
-                        p { "You are logged in - " a href="/admin" { "ADMIN" } }
-                    }
-                };
-
+        html {
+            head {
+                (NEWCSS)
+                script src="/index.js" {};
+                title { (CONFIG.get().unwrap().lock().await.instance_name) }
             }
 
-            @if can_view {
-                body {
-                    (render_monitor_list(false).await)
+            body {
+                header {
+                    h1 { (CONFIG.get().unwrap().lock().await.instance_name) }
+                    div style="position: absolute; top: 5px; right: 5px" {
+                        @if !is_logged_in {
+                            label for="password" { "Login: " }
+                            input #password placeholder="Password" type="password";
+                            button style="background: #181818" onclick="onLogin()" { "OK" }
+                        } @else {
+                            p { "You are logged in - " a href="/admin" { "ADMIN" } }
+                        }
+                    };
+
                 }
-            }
-            @else {
-                p { "Log in to see this" }
+
+                @if can_view {
+                    body {
+                        (render_monitor_list(false).await)
+                    }
+                }
+                @else {
+                    p { "Log in to see this" }
+                }
             }
         }
     };
@@ -206,97 +208,99 @@ pub async fn admin_template(cookies: CookieJar) -> (StatusCode, Markup) {
 
     let render = html!(
         (DOCTYPE);
-        head {
-            (NEWCSS);
-            script src="/admin.js" {};
-            title { (CONFIG.get().unwrap().lock().await.instance_name) }
-        }
-
-        body {
-            header {
-                h1 { (CONFIG.get().unwrap().lock().await.instance_name) " - Admin" }
+        html {
+            head {
+                (NEWCSS);
+                script src="/admin.js" {};
+                title { (CONFIG.get().unwrap().lock().await.instance_name) }
             }
-            p {
-                (render_monitor_list(true).await)
-                details {
-                    summary { "Add" };
-                    form #addform autocomplete="off" action="javascript:onAdd()" {
-                        label for="service-type" { "Type" }
-                        select #service-type onchange="onAddTypeChange()" {
-                            option value="tcp" selected { "TCP" }
-                            option value="http" { "HTTP" }
-                        }
 
-                        label for="service-name" { "Service name" }
-                        input #service-name placeholder="e.g. Website, Blog";
-
-                        label for="interval" { "Check interval" }
-                        input #interval type="number" placeholder="minutes" min="1" max="94080" value="10";
-
-                        label for="timeout" { "Timeout" }
-                        input #timeout type="number" placeholder="seconds" min="1" max="60" value="5";
-
-                        br;
-
-                        div #tcp-options {
-                            label for="sock-addr" { "Socket address" }
-                            input #sock-addr placeholder="133.33.33.37:4269";
-
-                            label for="expected-response" { "Expected response" }
-                            select #tcp-expected-response /* onchanged="onTcpExpectedResponseChange()" */ {
-                                option value="op" { "Open port" }
-                                option value="bytes" disabled { "Bytes" }
+            body {
+                header {
+                    h1 { (CONFIG.get().unwrap().lock().await.instance_name) " - Admin" }
+                }
+                p {
+                    (render_monitor_list(true).await)
+                    details {
+                        summary { "Add" };
+                        form #addform autocomplete="off" action="javascript:onAdd()" {
+                            label for="service-type" { "Type" }
+                            select #service-type onchange="onAddTypeChange()" {
+                                option value="tcp" selected { "TCP" }
+                                option value="http" { "HTTP" }
                             }
 
-                            // TODO: bytes input fields
-                        }
+                            label for="service-name" { "Service name" }
+                            input #service-name placeholder="e.g. Website, Blog";
 
-                        div #http-options hidden {
-                            label for="method" { "Method" }
-                            select #method {
-                                option value="get" { "GET" }
-                                option value="post" { "POST" }
-                                option value="put" { "PUT" }
-                                option value="delete" { "DELETE" }
-                                option value="options" { "OPTIONS" }
-                                option value="head" { "HEAD" }
-                                option value="trace" { "TRACE" }
-                                option value="connect" { "CONNECT" }
-                                option value="patch" { "PATCH" }
+                            label for="interval" { "Check interval" }
+                            input #interval type="number" placeholder="minutes" min="1" max="94080" value="10";
+
+                            label for="timeout" { "Timeout" }
+                            input #timeout type="number" placeholder="seconds" min="1" max="60" value="5";
+
+                            br;
+
+                            div #tcp-options {
+                                label for="sock-addr" { "Socket address" }
+                                input #sock-addr placeholder="133.33.33.37:4269";
+
+                                label for="expected-response" { "Expected response" }
+                                select #tcp-expected-response /* onchanged="onTcpExpectedResponseChange()" */ {
+                                    option value="op" { "Open port" }
+                                    option value="bytes" disabled { "Bytes" }
+                                }
+
+                                // TODO: bytes input fields
                             }
 
-                            label for="url" { "URL" }
-                            input #url placeholder="https://example.com";
+                            div #http-options hidden {
+                                label for="method" { "Method" }
+                                select #method {
+                                    option value="get" { "GET" }
+                                    option value="post" { "POST" }
+                                    option value="put" { "PUT" }
+                                    option value="delete" { "DELETE" }
+                                    option value="options" { "OPTIONS" }
+                                    option value="head" { "HEAD" }
+                                    option value="trace" { "TRACE" }
+                                    option value="connect" { "CONNECT" }
+                                    option value="patch" { "PATCH" }
+                                }
 
-                            label for="headers" { "Request headers" }
-                            textarea #headers {}
+                                label for="url" { "URL" }
+                                input #url placeholder="https://example.com";
 
-                            label for="expected-response" { "Expected response" }
-                            select #http-expected-response onchange="onHttpExpectedResponseChange()" {
-                                option value="any" { "Any" }
-                                option value="sc" { "Status codes" }
-                                option value="res" disabled { "Specific response" }
+                                label for="headers" { "Request headers" }
+                                textarea #headers {}
+
+                                label for="expected-response" { "Expected response" }
+                                select #http-expected-response onchange="onHttpExpectedResponseChange()" {
+                                    option value="any" { "Any" }
+                                    option value="sc" { "Status codes" }
+                                    option value="res" disabled { "Specific response" }
+                                };
+
+                                div #http-sc-options hidden {
+                                    label for="status-code" { "Status codes" }
+                                    input #status-code placeholder="200-299, 301, 400-410";
+                                };
+
+                                div #http-response-options hidden {
+                                    // TODO: front end for body checksum generation
+                                    label for="body-cs" { "Response body adler32 hash" }
+                                    input #body-cs type="number" placeholder="adler32" min="0" max="4294967296";
+                                };
+
+                                label for="request-body" { "Request body" }
+                                textarea #request-body {};
+
+
                             };
+                            br;
 
-                            div #http-sc-options hidden {
-                                label for="status-code" { "Status codes" }
-                                input #status-code placeholder="200-299, 301, 400-410";
-                            };
-
-                            div #http-response-options hidden {
-                                // TODO: front end for body checksum generation
-                                label for="body-cs" { "Response body adler32 hash" }
-                                input #body-cs type="number" placeholder="adler32" min="0" max="4294967296";
-                            };
-
-                            label for="request-body" { "Request body" }
-                            textarea #request-body {};
-
-
-                        };
-                        br;
-
-                        input type="submit";
+                            input type="submit";
+                        }
                     }
                 }
             }
@@ -365,21 +369,19 @@ async fn render_monitor_info(mon: Monitor, mon_id: u64) -> Markup {
                     }
                 }
 
-                @let first_record_time = records_last_30d.last().unwrap().time_checked;
-                tbody {
-                    tr {
-                        th scope="row" { "Current" }
-                        @let last_record = records_last_30d.first().unwrap();
-                        @let (msg, color) = result_to_text_color(&last_record.result);
-                        @let last_different_status = records.iter().find(|r| r.result != last_record.result);
-                        @let status_since = if let Some(ds) = last_different_status {
-                            ds.time_checked
-                        } else {
-                            records.last().unwrap().time_checked
-                        };
-                        td { span style={ "color:" (color) } { (msg) } " since " (time_util::time_diff_now(status_since as _)) }
-                        td { (last_record.response_time_ms.unwrap_or_default()) "ms" }
-                    }
+                //@let first_record_time = records_last_30d.last().unwrap().time_checked;
+                tr {
+                    th scope="row" { "Current" }
+                    @let last_record = records_last_30d.first().unwrap();
+                    @let (msg, color) = result_to_text_color(&last_record.result);
+                    @let last_different_status = records.iter().find(|r| r.result != last_record.result);
+                    @let status_since = if let Some(ds) = last_different_status {
+                        ds.time_checked
+                    } else {
+                        records.last().unwrap().time_checked
+                    };
+                    td { span style={ "color:" (color) } { (msg) } " since " (time_util::time_diff_now(status_since as _)) }
+                    td { (last_record.response_time_ms.unwrap_or_default()) "ms" }
                 }
 
                 // 24h
@@ -447,14 +449,17 @@ pub async fn monitor_template(monitor_id: Path<u64>, cookies: CookieJar) -> (Sta
 
     let Some(monitor) = database::monitor::get_by_id(*monitor_id).await else {
         let render = html!(
-            head {
-                (NEWCSS)
-                title { "Not found" }
-            }
+            (DOCTYPE)
+            html {
+                head {
+                    (NEWCSS)
+                    title { "Not found" }
+                }
 
-            body {
-                header { h1 { "404 Not Found" } }
-                p { "A monitor with this ID was not found" }
+                body {
+                    header { h1 { "404 Not Found" } }
+                    p { "A monitor with this ID was not found" }
+                }
             }
         );
 
@@ -462,27 +467,29 @@ pub async fn monitor_template(monitor_id: Path<u64>, cookies: CookieJar) -> (Sta
     };
 
     let render = html!(
-        head {
-            (NEWCSS)
-            @if can_view { title { "Monitor " (*monitor_id) } }
-            @else { title { "Unauthorized" } }
-        }
-
-        @if can_view {
-            header {
-                @let mon_name = if monitor.service_name.is_empty() {
-                    &monitor.service_data.service_location_str()
-                } else {
-                    &monitor.service_name
-                };
-                @let mon_name = mon_name.split_at_checked(24).map(|s| s.0).unwrap_or(&mon_name);
-
-                h1 { "Monitor info: " (mon_name) }
+        html {
+            head {
+                (NEWCSS)
+                @if can_view { title { "Monitor " (*monitor_id) } }
+                @else { title { "Unauthorized" } }
             }
-            (render_monitor_info(monitor, *monitor_id).await)
-        }
-        @else {
-            header { h1 { "Unauthorized" } }
+
+            @if can_view {
+                header {
+                    @let mon_name = if monitor.service_name.is_empty() {
+                        &monitor.service_data.service_location_str()
+                    } else {
+                        &monitor.service_name
+                    };
+                    @let mon_name = mon_name.split_at_checked(24).map(|s| s.0).unwrap_or(&mon_name);
+
+                    h1 { "Monitor info: " (mon_name) }
+                }
+                (render_monitor_info(monitor, *monitor_id).await)
+            }
+            @else {
+                header { h1 { "Unauthorized" } }
+            }
         }
     );
 
